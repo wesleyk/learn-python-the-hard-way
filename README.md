73dd8018c81ab7dc60d6de4e8d6898d105d10c37
Wesley Kim <wesleyjkim@gmail.com>
2012-05-26 21:24:38 -0700

    first few lessons

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 learn-python-the-hard-way
 =========================
 
-Exercises associated with http://learnpythonthehardway.org/book/. Learning Python!
+Exercises associated with http://learnpythonthehardway.org/book/. Learning Python
+
+Any exercise can be run with "python ex.py" in Terminal
---
+++ ex5.py
@@ -0,0 +1,16 @@
+name = 'Wesley Kim'
+age = 19
+height = 71 #inches
+weight = 155 #pounds
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+print "If I add %d, %d, and %d, I get %d." % (age, height, weight, age + height + weight)
